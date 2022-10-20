99733a614916d87db2032bc437bd6b3bbcd935e2
JHONATAN WILLIAN DOS SANTOS SILVA <u21686@academico.cotuca.unicamp.br>
2022-10-20 11:54:33 -0300

    login api

--- src/database/querys.js
+++ src/database/querys.js
@@ -10,7 +10,8 @@ module.exports = {
   deleteUsuario: "DELETE FROM Usuario WHERE idConta = @id",
   updateUsuarioById:"UPDATE Usuario SET nome = @nome,idade = @idade,@altura = altura,@email = email,@peso = peso,@altura = @altura,@senha = senha,@idGenero = idGenero,@idObjetivo = idObjetivo WHERE idConta = @id",
   getTotalProducts: "SELECT COUNT(*) FROM webstore.dbo.Products",
-  getProducById: "SELECT * FROM Products Where Id = @Id",
+  getProducById: "SELECT * FROM Usuario Where Id = @Id",
+  getUsuarioByEmail: "SELECT * FROM  Usuario Where email = @email",
 
   users: "SELECT * FROM Usuario Where email = @email"
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,78 @@
 const app = require('./app');
+const bodyParser = require('body-parser')
+
+const { getConnection, sql } = require("./database/connection");
+const querys = require("./database/querys");
+
+const jwt = require('jsonwebtoken')
+const SECRET = 'luiztools' 
+app.use(bodyParser.json());
+
+function verifyJWT(req,res,next){
+    const token = req.headers['x-access-token']
+    jwt.verify(token,SECRET,(err,decoded)=>{
+        if(err) return res.status(401).end();
+        req.userId = decoded.userId;
+        next();
+    })
+}
+
+// app.get('/api/clientes', verifyJWT, (req , res) => {
+//     //console.log(req.userI d + "fez está chamada!");
+//     try {
+//         const pool = await getConnection();
+//         const result = await pool.request().query(querys.getAllUsuarios);//aqui
+//         res.json(result.recordset);
+//       } catch (error) {
+//         res.status(500);
+//         res.send(error.message);
+//       }
+// })
+
+
+
+app.post('/api/login', async (req, res) => {
+    const {email,senha} = req.body;
+    // procurar no banco se tem alguem com esse email
+    try {
+        const pool = await getConnection();
+        const result = await pool
+                            .request()
+                            .input("email", sql.VarChar, email)
+                            .query(querys.getUsuarioByEmail);;
+
+        if (result.recordset.length == 0) {
+            res.status(404);
+            res.send("Usuário com este email não existe.")
+        } else {
+            console.log(result.recordset[0].senha == senha);
+            console.log(result.recordset[0].senha + " - " + senha)
+            if (result.recordset[0].senha == senha) {
+                res.status(200).json({ "message" : "Usuário logado com sucesso"});
+                return; 
+                // res.send
+            } 
+                
+            res.status(401).json({"message": "Senha incorreta."})
+            
+            /*
+            if (senha === result) {
+                const token = jwt.sign({userId:1},SECRET, {expiresIn:300});
+                return res.json({auth:true, token });
+            }
+            res.status(401).end();
+        */}
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+
+     
+})
+
+app.post('/api/logout', (req, res) => {
+    res.end();
+})
 
 app.listen(app.get("port"), () =>
     console.log("Server on port " + app.get("port"))
