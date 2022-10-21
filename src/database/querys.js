103ee5b0d8ece1650e0c19b64a54a599ad8bafd0
JHONATAN WILLIAN DOS SANTOS SILVA <u21686@academico.cotuca.unicamp.br>
2022-10-21 11:13:45 -0300

    algumas querys

--- src/database/querys.js
+++ src/database/querys.js
@@ -4,6 +4,9 @@ module.exports = {
   addNewExercicio:"INSERT INTO Exercicios(nome, quantVezes, sessoes,idAmbiente,idObjetivo) VALUES (@nome,@quantVezes,@sessoes,@idAmbiente,@idObjetivo);",
   deleteExercicio: "DELETE FROM Exercicios WHERE idExercicio = @id",
   updateExercicioById:"UPDATE Exercicios SET nome = @nome,quantVezes = @quantVezes,@sessoes = sessoes,@idAmbiente = idAmbiente,@idObjetivo = idObjetivo WHERE idExercicio = @id",
+  getExerc50_150:"SELECT DISTINCT Exercicios.nome FROM Exercicios where idExercicio BETWEEN 1 and 4",
+  getExerc60_160:"SELECT DISTINCT Exercicios.nome FROM Exercicios where idExercicio BETWEEN 4 and 8",
+  getExerc70_170:"SELECT DISTINCT Exercicios.nome FROM Exercicios where idExercicio BETWEEN 9 and 12",
   //USUARIOS
   getAllUsuarios: "SELECT * FROM Usuario", 
   addNewUsuario:"INSERT INTO Usuario(nome, idade, tempo, email,peso,altura ,senha,imagem,idGenero,idObjetivo) VALUES (@nome,@idade,@tempo,@email,@peso,@altura,@senha,@imagem,@genero,@objetivo);",
--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ app.post('/api/login', async (req, res) => {
         const result = await pool
                             .request()
                             .input("email", sql.VarChar, email)
-                            .query(querys.getUsuarioByEmail);;
+                            .query(querys.getUsuarioByEmail);
 
         if (result.recordset.length == 0) {
             res.status(404);
@@ -49,30 +49,60 @@ app.post('/api/login', async (req, res) => {
             console.log(result.recordset[0].senha + " - " + senha)
             if (result.recordset[0].senha == senha) {
                 res.status(200).json({ "message" : "Usuário logado com sucesso"});
+               
                 return; 
-                // res.send
             } 
                 
             res.status(401).json({"message": "Senha incorreta."})
-            
-            /*
-            if (senha === result) {
-                const token = jwt.sign({userId:1},SECRET, {expiresIn:300});
-                return res.json({auth:true, token });
-            }
-            res.status(401).end();
-        */}
+         }
     } catch (error) {
         res.status(500);
         res.send(error.message);
     }
-
-     
 })
+// app.get('/api/login', async (req, res) => {
+//     // procurar no banco se tem alguem com esse email
+//     try {
+//         const pool = await getConnection();
+//         const result = await pool.request().query(querys.getExerc50_150);
 
-app.post('/api/logout', (req, res) => {
-    res.end();
-})
+//         res.json(result.recordset);
+        
+//     } catch (error) {
+//         res.status(500);
+//         res.send(error.message);
+//     }
+// })
+// app.get('/api/login', async (req, res) => {
+//     // procurar no banco se tem alguem com esse email
+//     try {
+//         const pool = await getConnection();
+//         const result = await pool.request().query(querys.getExerc60_160);
+
+//         res.json(result.recordset);
+        
+//     } catch (error) {
+//         res.status(500);
+//         res.send(error.message);
+//     }
+// })
+// app.get('/api/login', async (req, res) => {
+//     // procurar no banco se tem alguem com esse email
+//     try {
+//         const pool = await getConnection();
+//         const result = await pool.request().query(querys.getExerc70_170);
+
+//         res.json(result.recordset);
+        
+//     } catch (error) {
+//         res.status(500);
+//         res.send(error.message);
+//     }
+// })
+
+// app.post('/api/logout', (req, res) => {
+//     res.end();
+// })
 
 app.listen(app.get("port"), () =>
     console.log("Server on port " + app.get("port"))
